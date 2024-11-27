59bef9b2eaca650317f08259848915d466ba444b
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2024-11-27 14:46:26 +0100

    code js terminé

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
 // *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+accordeon.addEventListener("click", (e) => {
+accordeon.querySelectorAll("details").forEach((det) => {
+    det.querySelector("summary").addEventListener("click", (e) => {
+        accordeon.querySelectorAll("details").forEach((autreDet) => {
+            if (autreDet !== det) {
+                autreDet.open = false;
+        }
+        });
+    });
+});
+});
